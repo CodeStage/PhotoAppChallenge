804671ca60260cb3eb472a63e031008089da0806
Christian König <christian@koenig.systems>
2015-10-27 17:09:39 +0100

    First experiment with collection view

---
+++ iGallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iGallery
+//
+//  Created by Christian König on 27.10.15.
+//  Copyright © 2015 CodeStage. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
