8a6941b5109e4274f948d3c85ceaf9e8e62c2ea1
Christian König <christian@koenig.systems>
2015-11-13 12:30:55 +0100

    Fixed a concurrency bug in the watch version

--- watchGalleryExtension/InterfaceController.swift
+++ watchGalleryExtension/InterfaceController.swift
@@ -18,14 +18,32 @@ class InterfaceController: WKInterfaceController {
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
         
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            self.loadPhotos()
+        }
+    }
+    
+    func loadPhotos() {
+        let sem = dispatch_semaphore_create(0)
         var items: [WKPickerItem] = []
+        
+        // Since the API is designed to load one image at a time but the picker needs the items
+        // all at once, we have to collect them and then pass them on
         for index in 0 ..< store.count {
             store.photoForIndex(index, completion: { (photo) -> () in
                 let item = WKPickerItem()
                 item.contentImage = WKImage.init(image: photo.image)
                 items.append(item)
+                dispatch_semaphore_signal(sem)
             })
         }
-        picker.setItems(items)
+        
+        for _ in 0 ..< store.count {
+            dispatch_semaphore_wait(sem, DISPATCH_TIME_FOREVER)
+        }
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.picker.setItems(items)
+        }
     }
 }
